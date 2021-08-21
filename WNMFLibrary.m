d052b5cca1fe38f328eb297f2b810ada0b0ac8df
kinan Abbas <kinan.magnamedia@gmail.com>
2021-08-21 16:41:04 +0200

    Second commit

--- WNMFLibrary.m
+++ WNMFLibrary.m
@@ -6,7 +6,7 @@ classdef WNMFLibrary
     
     methods (Static) 
         %%
-        function [ G , F ] = EM_WMU_NMF( W ,X , G , F , Iter_max_M_step, Iter_max_E_step )
+        function [X , G , F , t , initf , f] = EM_WMU_NMF( W ,X , G , F ,Iter_max_M_step , Iter_max_E_step , Nesterov_Max_Iterations , Nesterov_Min_Iterations )
             initf = norm( W.*(X-G*F) , 'fro' )^2;
             W2 = W.^2;
             [n1,n2]=size(W);
@@ -16,10 +16,10 @@ classdef WNMFLibrary
             for i=1:Iter_max_E_step
               WX = W2.*X;
               Xnew=WX+ (I-W).*(G*F); 
-
+              for j=1: Iter_max_M_step
               %M Step
               [ G , F ]=MU_NMF( Xnew , G , F , Iter_max_M_step );
-              X=Xnew;
+              end
             end
 
             t = toc;
@@ -28,27 +28,95 @@ classdef WNMFLibrary
 
         end
         %%
-        function [ G , F ] = EM_WNE_NMF( W ,X , G , F , Iter_max_E_step )
+        function [X , G, F , t , initf , f] = EM_WNE_NMF( W ,X , G , F ,  Iter_max_M_step , Iter_max_E_step , Nesterov_Max_Iterations , Nesterov_Min_Iterations  )
+            initf = norm( W.*(X-G*F) , 'fro' )^2;
+            W2 = W.^2;
+            WX = W2.*X;
+            [n1,n2]=size(W);
+            I=ones(n1,n2);
+            reducedDim=size(G,2);
+            tic;
+            %E Step
+            for i=1:Iter_max_E_step 
+              Result=WX+ (I-W).*(G*F); 
+                for j=1:Iter_max_M_step
+                  %M Step
+                  [G , F ,it,ela,HIS]=NeNMF(Result,reducedDim,'W_INIT',G,'H_INIT',F,'MAX_ITER',Nesterov_Max_Iterations,'MIN_ITER',Nesterov_Min_Iterations);
+                end
+            end
+            t = toc;
+            f = norm( W.*(Result-G*F) , 'fro' )^2;
+            fprintf('\n### Elapse time: %d sec.\n###   Initial objective value: %d\n###   Objective value: %d \n' , t , initf , f );
+            
+        end
+
+            %%
+        function [X , G, F , t , initf , f] = EM_L1R_WNE_NMF( W ,X , G , F , Iter_max_M_step , Iter_max_E_step , Nesterov_Max_Iterations , Nesterov_Min_Iterations  )
             initf = norm( W.*(X-G*F) , 'fro' )^2;
             W2 = W.^2;
             [n1,n2]=size(W);
             I=ones(n1,n2);
-            reducedDim=50;
+            reducedDim=size(G,2);
             tic;
             %E Step
             for i=1:Iter_max_E_step
               WX = W2.*X;
               Xnew=WX+ (I-W).*(G*F); 
-
+               
               %M Step
-              [G , F ,it,ela,HIS]=NeNMF(Xnew,reducedDim,'TYPE','L1R');
-              X=Xnew;
+               for j=1:Iter_max_M_step
+                [G , F ,it,ela,HIS]=NeNMF(Xnew,reducedDim,'TYPE','L1R','W_INIT',G,'H_INIT',F,'MAX_ITER',Nesterov_Max_Iterations,'MIN_ITER',Nesterov_Min_Iterations);
+               end
             end
             t = toc;
             f = norm( W.*(X-G*F) , 'fro' )^2;
             fprintf('\n### Elapse time: %d sec.\n###   Initial objective value: %d\n###   Objective value: %d \n' , t , initf , f );
         end
 
+          %%
+        function [X , G, F , t , initf , f] = EM_L2R_WNE_NMF( W ,X , G , F  ,Iter_max_M_step , Iter_max_E_step , Nesterov_Max_Iterations , Nesterov_Min_Iterations )
+            initf = norm( W.*(X-G*F) , 'fro' )^2;
+            W2 = W.^2;
+            [n1,n2]=size(W);
+            I=ones(n1,n2);
+            reducedDim=size(G,2);
+            tic;
+            %E Step
+            for i=1:Iter_max_E_step
+              WX = W2.*X;
+              Xnew=WX+ (I-W).*(G*F); 
+              for j=1:Iter_max_M_step
+              %M Step
+              [G , F ,it,ela,HIS]=NeNMF(Xnew,reducedDim,'TYPE','L2R','W_INIT',G,'H_INIT',F,'MAX_ITER',Nesterov_Max_Iterations,'MIN_ITER',Nesterov_Min_Iterations);
+              end 
+            end
+            t = toc;
+            f = norm( W.*(X-G*F) , 'fro' )^2;
+            fprintf('\n### Elapse time: %d sec.\n###   Initial objective value: %d\n###   Objective value: %d \n' , t , initf , f );
+        end
+
+    %%
+        function [X , G, F , t , initf , f] = EM_GWNE_NMF( W ,X , G , F , Iter_max_M_step , Iter_max_E_step , Nesterov_Max_Iterations , Nesterov_Min_Iterations  )
+            initf = norm( W.*(X-G*F) , 'fro' )^2;
+            W2 = W.^2;
+            [n1,n2]=size(W);
+            I=ones(n1,n2);
+            reducedDim=size(G,2);
+            tic;
+            S=constructW(X);
+            %E Step
+            for i=1:Iter_max_E_step
+              WX = W2.*X;
+              Xnew=WX+ (I-W).*(G*F); 
+              for j=1:Iter_max_M_step
+              %M Step
+              [G , F ,it,ela,HIS]=NeNMF(Xnew,reducedDim,'TYPE','MR','S_MTX',S,'W_INIT',G,'H_INIT',F,'MAX_ITER',Nesterov_Max_Iterations,'MIN_ITER',Nesterov_Min_Iterations);
+              end
+            end
+            t = toc;
+            f = norm( W.*(X-G*F) , 'fro' )^2;
+            fprintf('\n### Elapse time: %d sec.\n###   Initial objective value: %d\n###   Objective value: %d \n' , t , initf , f );
+        end
     end
 end
 
--- main.m
+++ main.m
@@ -2,20 +2,22 @@
 clear
 clc
 rng(1)
+X=readmatrix('FinalMatrix_100.txt');
+r = 2; % rank of the data matrix X
+m = size(X,1); n = size(X,2); % size of the data matrix X (m x n)
 
-r = 4; % rank of the data matrix X
-m = 500; n = 500; % size of the data matrix X (m x n)
 
-Gtheo = rand(m,r); % simulation of theoretical matrix Gtheo
-Ftheo = rand(r,n); % simulation of theoretical matrix Ftheo
-Xtheo = Gtheo*Ftheo; % simulation of theoretical data matrix Xtheo
+Gtheo = round(255.*rand(m,r));
+Ftheo = rand(r,n);
+
+% Xtheo = Gtheo*Ftheo; % simulation of theoretical data matrix Xtheo
 
 N = zeros(m,n); % simulating noise matrice N
-X = Xtheo+N; % simulating data matrix X
+% X = Xtheo+N; % simulating data matrix X
 
 
 %% Matrix initialisation
-Ginit = rand(m,r);
+Ginit = round(255.*rand(m,r));
 Finit = rand(r,n);
 
 
@@ -32,17 +34,35 @@ W(idx_missing(1:round(prop_missing*m*n))) = 0;
 X = X.*W;
 
 %% Running EM_WMU_NMF
-Iter_max_E_step=10;
+Iter_max_E_step=2;
+Iter_max_M_step=5;
+Nesterov_Max_Iterations=100;
+Nesterov_Min_Iterations=10;
 % WMU_NMF
-fprintf('\nRunning NMF Lee and Seung multiplicative update...');
+% fprintf('\nRunning NMF Lee and Seung multiplicative update...');
 % [ G_WMU_NMF , F_WMU_NMF ] = WMU_NMF( W , X , Ginit , Finit , Iter_max );
 
 % EM_WMU_NMF
-fprintf('\nRunning EM_WMU_NMF...');
-[ G_EM_WMU_NMF , F_EM_WMU_NMF ] = WNMFLibrary.EM_WMU_NMF( W , X , Ginit , Finit , Iter_max,Iter_max_E_step );
+fprintf('\n Running EM_WMU_NMF...');
+% [EM_WMU_NMF,G_EM_WMU_NMF,F_EM_WMU_NMF,T_EM_WMU_NMF,I_EM_WMU_NMF,f_EM_WMU_NMF]  = WNMFLibrary.EM_WMU_NMF( W , X , Ginit , Finit , Iter_max,Iter_max_E_step );
 
 % EM_WNE_NMF
 fprintf('\nRunning EM_WNE_NMF...');
-[ G_EM_WNE_NMF , F_EM_WNE_NMF ] = WNMFLibrary.EM_WNE_NMF( W , X , Ginit , Finit ,Iter_max_E_step );
+[EM_WNE_NMF,G_EM_WNE_NMF,F_EM_WNE_NMF,T_EM_WNE_NMF,I_EM_WNE_NMF,f_EM_WNE_NMF]  = WNMFLibrary.EM_WNE_NMF( W , X , Ginit , Finit , Iter_max_M_step,Iter_max_E_step,Nesterov_Max_Iterations,Nesterov_Min_Iterations );
 
 
+
+% EM_L1R_WNE_NMF_
+fprintf('\nRunning EM_L1R_WNE_NMF_...');
+[L1R_WNE_NMF, G_EM_L1R_WNE_NMF,F_EM_L1R_WNE_NMF, T_EM_L1R_WNE_NMF , I_EM_L1R_WNE_NMF , f_EM_L1R_WNE_NMF] = WNMFLibrary.EM_L1R_WNE_NMF( W , X , G_EM_WNE_NMF , F_EM_WNE_NMF ,Iter_max_M_step,Iter_max_E_step,Nesterov_Max_Iterations,Nesterov_Min_Iterations  );
+
+% EM_GWNE_NMF
+% fprintf('\nRunning EM_GWNE_NMF...');
+% EM_GWNE_NMF  = WNMFLibrary.EM_GWNE_NMF( W , X , Ginit , Finit ,Iter_max_M_step,Iter_max_E_step,Nesterov_Max_Iterations,Nesterov_Min_Iterations  );
+
+fprintf ('\n ### Matrix size is %d x %d ' ,m,n);
+fprintf (' \n ### Nesterov_Max_Iterations= % d ,  Nesterov_Min_Iterations = %d, Iter_max_E_step = %d , Iter_max_M_step= %d ',Nesterov_Max_Iterations,Nesterov_Min_Iterations,Iter_max_E_step,Iter_max_M_step);
+% fprintf (' \n ### EM_WMU_NMF, Time = %d , Initial value = %d, Objective value = %d  ',real(T_EM_WMU_NMF),real(I_EM_WMU_NMF),real(f_EM_WMU_NMF)); 
+fprintf (' \n ### EM_WNE_NMF, Time = %d , Initial value = %d, Objective value = %d  ',real(T_EM_WNE_NMF),real(I_EM_WNE_NMF),real(f_EM_WNE_NMF)); 
+fprintf (' \n ### EM_L1R_WNE_NMF, Time = %d , Initial value = %d, Objective value = %d  ',real(T_EM_L1R_WNE_NMF),real(I_EM_L1R_WNE_NMF),real(f_EM_L1R_WNE_NMF)); 
+fprintf('\n');
